9d3c3735c56ff4a3522f243300f1e3adaa4fb961
Flavio Rossi <flaviowenzel1996@gmail.com>
2016-09-25 02:21:08 -0300

    Estrutura de endereço criada

--- PAPProjJPA/src/papprojjpa/PAPProjJPA.java
+++ PAPProjJPA/src/papprojjpa/PAPProjJPA.java
@@ -5,9 +5,11 @@
  */
 package papprojjpa;
 
+import br.com.euemais3.dao.EnderecoUsuarioDAO;
 import br.com.euemais3.dao.FilmeDAO;
 import br.com.euemais3.dao.TipoContaDAO;
 import br.com.euemais3.dao.UsuarioDAO;
+import br.com.euemais3.model.EnderecoUsuario;
 import br.com.euemais3.model.Filme;
 import br.com.euemais3.model.TipoConta;
 import br.com.euemais3.model.Usuario;
@@ -24,22 +26,33 @@ public class PAPProjJPA {
      */
     public static void main(String[] args) {
         try {
-            TipoContaDAO daoTipoContaDAO = new TipoContaDAO();
+            TipoContaDAO daoTipoConta = new TipoContaDAO();
             UsuarioDAO daoUsuario = new UsuarioDAO();
+            EnderecoUsuarioDAO daoEnderecoUsuario = new EnderecoUsuarioDAO();
             FilmeDAO daoFilme = new FilmeDAO();
+            
             
             TipoConta tipoConta = new TipoConta(null, "ADM-000", "Conta de administração");
             Usuario usuario = new Usuario(null, "User1", "user", "1", new Date("10/10/2010"), tipoConta);
+            EnderecoUsuario enderecoUsuario = new EnderecoUsuario(null, "Rua Jão", "15A","Jardim foguete", "", "0985480", "SP", "São Benedito da Cruz");
             Filme filme = new Filme(null, "Gravidade", "Otomar no cu", new Date("10/06/2013"), "Dirigid puta que pariu", "Não sei", new Date("01/01/01"), "Ação", "sadsd", "EUA", "English", "Colorido", "Dolby");
+                        
             
-            tipoConta = daoTipoContaDAO.salvar(tipoConta);
-            usuario = daoUsuario.salvar(usuario);            
+            tipoConta = daoTipoConta.salvar(tipoConta);
+            usuario = daoUsuario.salvar(usuario);
+            enderecoUsuario = daoEnderecoUsuario.salvar(enderecoUsuario);
             filme = daoFilme.salvar(filme);
 
             
             System.out.println("ID do tipo de conta salvo: " + tipoConta.getId());
             System.out.println("ID do usuario salvo: " + usuario.getId());
+            System.out.println("ID do endereço do usuario salvo: " + enderecoUsuario.getId());
             System.out.println("ID do filme salvo: " + filme.getId());
+            
+            
+            daoFilme.excluir(filme.getId());
+            daoUsuario.excluir(usuario.getId());
+            daoTipoConta.excluir(tipoConta.getId());
             
 
             
