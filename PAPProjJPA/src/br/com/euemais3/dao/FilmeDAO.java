7346a7450542e48ce198e944839953c0018bc224
Flavio Rossi <flaviowenzel1996@gmail.com>
2016-09-24 23:11:56 -0300

    DAO filme criada

---
+++ PAPProjJPA/src/br/com/euemais3/dao/FilmeDAO.java
@@ -0,0 +1,52 @@
+package br.com.euemais3.dao;
+
+import br.com.euemais3.model.Filme;
+import javax.persistence.EntityManager;
+
+public class FilmeDAO extends DAOBase{
+
+    public Filme salvar(Filme filme) {
+        EntityManager entityManager = getEntityManager();
+
+        try {
+            entityManager.getTransaction().begin();
+            if (filme.getId() == null) {
+                entityManager.persist(filme);
+            } else {
+                filme = entityManager.merge(filme);
+            }
+            entityManager.getTransaction().commit();
+        } finally {
+//            entityManager.close();
+        }
+        return filme;
+    }
+
+    public void excluir(Long id) {
+        EntityManager entityManager = getEntityManager();
+
+        try {
+            entityManager.getTransaction().begin();
+
+            Filme usuario = entityManager.find(Filme.class, id);
+            entityManager.remove(usuario);
+
+            entityManager.getTransaction().commit();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public Filme consultarPorId(Long id) {
+        EntityManager entityManager = getEntityManager();
+        Filme usuario = null;
+
+        try {
+            usuario = entityManager.find(Filme.class, id);
+        } finally {
+            entityManager.close();
+        }
+        return usuario;
+    }
+
+}
