78402ed883efefdf1a7a09e6069e93136262d8de
ume8cuo <ume8cuo@VWBRANCDL2164.sa.vwg>
2016-09-30 12:51:41 -0300

    EJBs criadas

---
+++ PAPProjEJB/src/java/br/com/euemais3/ejb/SalaEJB.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.euemais3.ejb;
+
+/**
+ *
+ * @author ume8cuo
+ */
+public class SalaEJB {
+    
+}
